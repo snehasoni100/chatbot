065d2233bb8cefa109ee0328b07d6511da65aaa5
Sneha <sonisneha1001@gmail.com>
2022-07-07 20:15:49 +0530

    complete

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:chatbot/home.dart';
+import 'package:flutter/material.dart';
+import 'dart:ui' as ui;
+void main() {
+  runApp(
+      MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: Home(),
+  ));
+}
+
